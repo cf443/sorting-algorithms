3679254321f98df6e74e2b62a6040c35db66c877
Craig Ferguson <mediocrecraig@gmail.com>
2017-06-27 23:45:24 +0100

    Bubble sort implementation

--- src/uk/ac/cam/cf443/algs/BubbleSort.java
+++ src/uk/ac/cam/cf443/algs/BubbleSort.java
@@ -1,5 +1,32 @@
 package uk.ac.cam.cf443.algs;
 
-public class BubbleSort {
+import uk.ac.cam.cf443.core.*;
 
+public final class BubbleSort extends ConcreteSortAlgorithm {
+	
+	//Singleton instance and private constructor
+	public static final SortAlgorithm INSTANCE = new BubbleSort();
+	private BubbleSort() {super("Bubble sort");}
+	
+	/**
+	 * The bubble sort algorithm, sorts by running N many scans through the array and swapping
+	 * adjacent elements if they are out of order. The last element in each pass must be the
+	 * largest element of the array, and therefore the range of the array to be sorted decreases
+	 * by 1 each time.
+	 * 
+	 * Time complexity: O(n^2)
+	 */
+	@Override
+	public void sort(SortArray array) {
+		
+		for (int i = array.getLength(); i >= 1; i--) {
+			for (int j = 0; j < i - 1 ; j++) {
+				
+				array.compareAndSwap(i, j+1);
+				array.setElement(i - 1, SortArray.ElementState.DONE);
+			}
+		}
+		
+	}
+	
 }
