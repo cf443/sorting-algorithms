fd2e652d230a0555295df035c5c47382b3d7e9ad
Craig Ferguson <mediocrecraig@gmail.com>
2017-06-29 21:21:29 +0100

    CocktailSort: Additional documentation

--- src/uk/ac/cam/cf443/algs/CocktailSort.java
+++ src/uk/ac/cam/cf443/algs/CocktailSort.java
@@ -1,5 +1,45 @@
 package uk.ac.cam.cf443.algs;
 
-public class CocktailSort {
+import uk.ac.cam.cf443.core.*;
 
+public final class CocktailSort extends ConcreteSortAlgorithm {
+	
+	//Singleton instance and private constructor
+	public static final SortAlgorithm INSTANCE = new CocktailSort();
+	private CocktailSort() {super("Cocktail sort");}
+	
+	
+	/**
+	 * The Cocktail shaker sort. A variant of bubble sort which scans the array in both directions
+	 * on each pass through the list. The algorithm provides a marginal improvement over
+	 * bubble sort, as it solves the problem of small elements taking many passes to reach the
+	 * front of the list (turtles).
+	 * 
+	 *  Time complexity: O(n^2)
+	 */
+	@Override
+	public void sort(SortArray array) {
+		
+		int left = 0,
+			right = array.getLength(),
+			i = 0;
+		
+		while (left < right) {
+			
+			//Scan right
+			for (; i+1 < right; i++) array.compareAndSwap(i, i+1);
+			array.setElement(i, SortArray.ElementState.DONE);
+			
+			right--; i--;
+			
+			if (left == right) break;
+			
+			//Scan left
+			for (; i > left; i--) array.compareAndSwap(i-1, i);
+			array.setElement(i, SortArray.ElementState.DONE);
+			
+			left++; i++;
+		}
+		
+	}
 }
